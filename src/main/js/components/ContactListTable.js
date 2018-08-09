01d7655ec6cb9706e74db3df455f716e633d93ce
Kari Jarnot <jarnotkari@gmail.com>
2018-08-09 10:16:14 -0500

    Formatted

--- src/main/js/components/ContactListTable.js
+++ src/main/js/components/ContactListTable.js
@@ -10,76 +10,75 @@ class ContactListTable extends React.Component {
 			
 	render() {
 		const {contactEntities} = this.props;
-			
 		return (
 			<div>
 				<ReactTable
 			          data={contactEntities}
-			        
 			          noDataText="No Contacts Loaded"
-			          columns={[
-			            {
-			              Header: "Name",
-			              columns: [
-			                {
-			                  Header: "First Name",
-			                  accessor: "firstName",
-			                  filterable: true
-			                },
-			                {
-			                  Header: "Last Name",
-			                  accessor: "lastName",
-			                	  filterable: true
-			                }
-			              ]
-			            },
-			            {
-			              Header: "Info",
-			              columns: [
-			                {
-			                  Header: "Phone Number",
-			                  accessor: "phoneNumber"
-			                },
-			                {
-			                  Header: "Email Address",
-			                  accessor: "email"
-			                }
-			              ]
-			            }
-			            ,
-			            {
-			              Header: "",
-			              columns: [
-			            	  {
-				                  Header: "",
-				                  Cell: ({row, original}) => (
-				                  <UpdateContactButton contact={original} onSelectForUpdate={this.props.onSelectForUpdate} />
-				                 )
-				                },
-			            	  {
-			                  Header: "",
-			                  Cell: ({row, original}) => (
-			                  <DeleteContactButton contact={original} onDelete={this.props.onDelete} />
-			                 )
-			                }
-			                
-			              ]
-			            }
-			          ]}
-			        
-			        defaultSorted={[
-			            {
-			              id: "lastName",
-			              desc: false
-			            }
-			          ]}
-			          defaultPageSize={5}
-			          className="-striped -highlight"
-			        />
-				
-		        </div>
-
-			
+			          columns={
+						[
+							{
+								Header: "Name",
+								columns: 
+								[
+					                {
+					                  Header: "First Name",
+					                  accessor: "firstName",
+					                  filterable: true
+					                },
+					                {
+					                  Header: "Last Name",
+					                  accessor: "lastName",
+					                	  filterable: true
+					                }
+					             ]
+							},
+							{
+								Header: "Info",
+								columns: 
+								[
+									{
+										Header: "Phone Number",
+										accessor: "phoneNumber"
+									},
+									{
+					                  Header: "Email Address",
+					                  accessor: "email"
+					                }
+								]
+							},
+							{
+								Header: "",
+								columns: 
+								[
+									{
+										Header: "",
+										Cell: ({row, original}) => (
+											<UpdateContactButton contact={original} onSelectForUpdate={this.props.onSelectForUpdate} />
+										)
+									},
+									{
+										Header: "",
+										Cell: ({row, original}) => (
+											<DeleteContactButton contact={original} onDelete={this.props.onDelete} />
+										)
+									}
+								]
+							}
+						]
+					}
+			        defaultSorted={
+						[
+				            {
+				              id: "lastName",
+				              desc: false
+				            }
+				         ]
+					}
+			        defaultPageSize={5}
+			        className="-striped -highlight"
+			    />
+	        </div>
 		);
 	}
 }
